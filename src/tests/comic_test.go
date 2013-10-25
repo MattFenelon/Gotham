4d8b537e3d377210c7c5c411c6c2993f36c382ec
Matt Fenelon <matt@fnln.me>
2013-10-25 16:06:04 +0100

    New test for adding multiple comics.
    Ensures that the code for adding an event isn't hardcoded.

--- src/tests/comic_test.go
+++ src/tests/comic_test.go
@@ -20,6 +20,24 @@ func TestCreateComic(t *testing.T) {
 	AssertEquality(t, expected, repo)
 }
 
+func TestCreateMultipleComics(t *testing.T) {
+	id := uuid.NewRandom()
+	repo := NewFakeComicRepository()
+	expected := []*domain.ComicAdded{
+		domain.NewComicAdded(id.String(), "Prophet", "Prophet 31"),
+		domain.NewComicAdded(id.String(), "Batman", "Batman 1")}
+
+	t.Log("When adding multiple comics")
+	command := domainservices.NewCreateComicCommand(id, "Prophet", "Prophet 31")
+	domainservices.AddComic(command, repo) // TODO: Refactor to command processor
+
+	command2 := domainservices.NewCreateComicCommand(id, "Batman", "Batman 1")
+	domainservices.AddComic(command2, repo) // TODO: Refactor to command processor
+
+	t.Log("\tIt should raise a comic added event for all of the added comics")
+	AssertCollectionEquality(t, expected, repo)
+}
+
 type FakeComicRepository struct {
 	events []*domain.ComicAdded // should be able to contain other types of event
 }
@@ -33,18 +51,37 @@ func (repo *FakeComicRepository) AddEvent(event *domain.ComicAdded) {
 }
 
 func AssertEquality(t *testing.T, expected *domain.ComicAdded, actual *FakeComicRepository) {
-	values := make([]domain.ComicAdded, 0, len(actual.events))
+	AssertCollectionEquality(t, []*domain.ComicAdded{expected}, actual)
+}
+
+func AssertCollectionEquality(t *testing.T, expected []*domain.ComicAdded, actual *FakeComicRepository) {
+	actualValues := make([]domain.ComicAdded, 0, len(actual.events))
+	expectedValues := make([]domain.ComicAdded, 0, len(expected))
+
+	for _, expectedEvent := range expected {
+		expectedValues = append(expectedValues, *expectedEvent)
+	}
+
 	error := false
-	for _, event := range actual.events {
-		values = append(values, *event)
-		if *event != *expected {
+	for _, actualEvent := range actual.events {
+		actual := *actualEvent
+		actualValues = append(actualValues, actual)
+
+		found := false
+		for _, expectedValue := range expectedValues {
+			if actual == expectedValue {
+				found = true
+			}
+		}
+		if found == false {
 			error = true
 		}
 	}
 	if error {
 		t.Errorf("\tCollection was expected to only contain")
-		t.Errorf("\t\t%#v\n", expected)
+		t.Errorf("\t\t%#v\n", expectedValues)
 		t.Errorf("\t\tbut contained\n")
-		t.Errorf("\t\t%#v", values)
+		t.Errorf("\t\t%#v", actualValues)
 	}
+
 }
