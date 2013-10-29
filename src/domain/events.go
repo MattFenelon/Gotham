b40f889a520af3bb2eb9391cd99912b9ad4b04ce
Matt Fenelon <matt@fnln.me>
2013-10-29 11:22:34 +0000

    Assertions are now made using a custom Equal method on the type.
    A custom method allows for each type to decide what equality means. It also gets round the problem of comparing complex types that can't be compared using the == operator, such as slices.

--- src/domain/events.go
+++ src/domain/events.go
@@ -1,5 +1,9 @@
 package domain
 
+import (
+	"fmt"
+)
+
 type ComicAdded struct {
 	Id          comicId // TODO: Create an identifier type
 	SeriesTitle seriesTitle
@@ -8,4 +12,20 @@ type ComicAdded struct {
 
 func NewComicAdded(comicId comicId, seriesTitle seriesTitle, bookTitle bookTitle) *ComicAdded {
 	return &ComicAdded{Id: comicId, SeriesTitle: seriesTitle, BookTitle: bookTitle}
+}
+
+func (a ComicAdded) Equal(b interface{}) bool {
+	if r, ok := b.(ComicAdded); ok {
+		return a.EqualTo(r)
+	}
+
+	return false
+}
+
+func (a ComicAdded) EqualTo(b ComicAdded) bool {
+	return a.Id.Equal(b.Id) && a.SeriesTitle == b.SeriesTitle && a.BookTitle == b.BookTitle
+}
+
+func (ca ComicAdded) String() string {
+	return fmt.Sprintf("%T:{id: %v, seriesTitle: \"%v\", bookTitle: \"%v\"}", ca, ca.Id, ca.SeriesTitle, ca.BookTitle)
 }
--- src/domain_tests/asserts.go
+++ src/domain_tests/asserts.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
+type equaler interface {
+	Equal(interface{}) bool
+}
+
 func AssertEquality(t *testing.T, expected *domain.ComicAdded, actual *FakeEventStorer) {
 	AssertCollectionEquality(t, []*domain.ComicAdded{expected}, actual)
 }
@@ -25,23 +29,22 @@ func AssertCollectionIsEmpty(t *testing.T, actual *FakeEventStorer) {
 }
 
 func AssertCollectionEquality(t *testing.T, expected []*domain.ComicAdded, actual *FakeEventStorer) {
-	// Write tests to test the assert methods
-	actualValues := make([]domain.ComicAdded, 0, len(actual.events))
-	expectedValues := make([]domain.ComicAdded, 0, len(expected))
+	actualValues := make([]equaler, 0, len(actual.events))
+	expectedValues := make([]equaler, 0, len(expected))
 
 	for _, expectedEvent := range expected {
-		expectedValues = append(expectedValues, *expectedEvent)
+		expectedValues = append(expectedValues, equaler(*expectedEvent))
 	}
 
 	error := false
 	for _, actualEvent := range actual.events {
 		actual := *actualEvent
-		actualValues = append(actualValues, actual)
+		actualValues = append(actualValues, equaler(actual))
 
 		found := false
 		for _, expectedValue := range expectedValues {
-			if actual == expectedValue {
-				found = true
+			if found = actual.Equal(expectedValue); found {
+				break
 			}
 		}
 		if found == false {
@@ -50,8 +53,8 @@ func AssertCollectionEquality(t *testing.T, expected []*domain.ComicAdded, actua
 	}
 	if error {
 		t.Errorf("\tCollection was expected to only contain")
-		t.Errorf("\t\t%#v\n", expectedValues)
+		t.Errorf("\t\t%v\n", expectedValues)
 		t.Errorf("\t\tbut contained\n")
-		t.Errorf("\t\t%#v", actualValues)
+		t.Errorf("\t\t%v", actualValues)
 	}
 }
