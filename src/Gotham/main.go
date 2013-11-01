8eaafb1b7f26da8b686ff39ffaad87dfd924c333
Matt Fenelon <matt@fnln.me>
2013-11-01 10:41:34 +0000

    Hooked up the web app to Riak.

--- src/Gotham/main.go
+++ src/Gotham/main.go
@@ -4,10 +4,11 @@ import (
 	"gotham/lib"
 	"log"
 	"net/http"
+	"persistence/riak"
 )
 
 func main() {
-	exports := lib.Configure(nil)
+	exports := lib.Configure(riak.NewRiakEventStore([]string{"127.0.0.1:8087"}, "httpapi"))
 
 	err := http.ListenAndServe(":7001", exports.Handler)
 	if err != nil {
