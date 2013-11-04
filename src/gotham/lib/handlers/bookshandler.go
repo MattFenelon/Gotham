fbbc11e117ff77b40361da384e5f5f6e3963ea81
Matt Fenelon <matt@fnln.me>
2013-11-04 14:27:52 +0000

    Changing the /books endpoint to accept images as well as the comic metadata.
    http_tests are failing at this point.

--- src/gotham/lib/handlers/bookshandler.go
+++ src/gotham/lib/handlers/bookshandler.go
@@ -4,7 +4,10 @@ import (
 	"code.google.com/p/go-uuid/uuid"
 	"domainservices"
 	"encoding/json"
+	"mime"
+	"mime/multipart"
 	"net/http"
+	"strings"
 )
 
 type booksPostRequest struct {
@@ -18,13 +21,31 @@ func BooksHandler(w http.ResponseWriter, r *http.Request, storer domainservices.
 		return
 	}
 
-	if r.Header.Get("Content-Type") != "application/json" {
-		w.WriteHeader(http.StatusUnsupportedMediaType)
-		return
-	}
+	_, params, _ := mime.ParseMediaType(r.Header.Get("Content-Type"))
+	// if err != nil || mediaType != "multipart/form-data" {
+	// 	w.WriteHeader(http.StatusUnsupportedMediaType)
+	// 	return
+	// }
+	boundary := params["boundary"]
+	reader := multipart.NewReader(r.Body, boundary)
+	// 20KB is an arbitary choice for max bytes. This API doesn't need to be quick so writing the image files
+	// to disk isn't a concern and it ensures the server doesn't take up too much memory.
+	form, _ := reader.ReadForm(20480)
+	defer func() {
+		// form.RemoveAll() // TODO: Log errors
+	}()
+	// if err != nil {
+	// }
+	// if form.Value["metadata"] == nil || len(form.Value["metadata"]) == 0 {
+	// }
+	// Ignore extra values or return an error?
 
-	dec := json.NewDecoder(r.Body)
-	defer r.Body.Close()
+	metadata := form.Value["metadata"][0]
+	// if metadata == "" {
+	// }
+
+	metadataReader := strings.NewReader(metadata)
+	dec := json.NewDecoder(metadataReader)
 
 	var request booksPostRequest
 	if err := dec.Decode(&request); err != nil {
--- src/http_test/books_test.go
+++ src/http_test/books_test.go
@@ -1,10 +1,14 @@
 package http_test
 
 import (
+	"bytes"
 	"gotham/lib"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"net/textproto"
+	"path/filepath"
 	"persistence"
 	"strings"
 	"testing"
@@ -18,11 +22,18 @@ func TestAddBook(t *testing.T) {
 	server := httptest.NewServer(exports.Handler)
 	defer server.Close()
 
-	reader := strings.NewReader(`{
+	metadata := `{
 	"seriesTitle": "Prophet",
 	"title": "Prophet 31"
-}`)
-	rsp, err := http.Post(server.URL+"/books", "application/json", reader)
+}`
+
+	var buffer bytes.Buffer
+	writer := multipart.NewWriter(&buffer)
+	writeMetadata(writer, metadata)
+	writeImageData(t, writer, "testdata\\Prophet 30 Cover Image.jpg")
+	writer.Close()
+
+	rsp, err := http.Post(server.URL+"/books", writer.FormDataContentType(), &buffer)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -46,41 +57,9 @@ func TestAddBook(t *testing.T) {
 	if len(actualEvents) == 0 || actualEvents[0].Title != "Prophet 31" {
 		t.Errorf("\tExpected 1 items but contained %v", actualEvents)
 	}
-}
 
-func TestAddBookWithInvalidJSON(t *testing.T) {
-	t.Log("POST /books")
-
-	store := persistence.NewInMemoryEventStore()
-	exports := lib.Configure(store)
-	server := httptest.NewServer(exports.Handler)
-	defer server.Close()
-
-	reader := strings.NewReader(`complete-and-utter-rubbish`)
-	rsp, err := http.Post(server.URL+"/books", "application/json", reader)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	defer rsp.Body.Close()
-	bodyBytes, _ := ioutil.ReadAll(rsp.Body)
-	body := string(bodyBytes)
-
-	t.Log("The response should be 400 Bad Request")
-	if rsp.StatusCode != 400 {
-		t.Errorf("\tExpected 400 but was %v", rsp.StatusCode)
-	}
-
-	t.Log("The response body should be empty")
-	if body != "" {
-		t.Errorf("\tExpected \"\" but was %v", body)
-	}
-
-	t.Log("The comic should not be persisted")
-	actualEvents := store.GetAllEvents()
-	if len(actualEvents) != 0 {
-		t.Errorf("\tExpected 0 items but contained %v", actualEvents)
-	}
+	t.Log("The comic pages should be persisted")
+	t.Error("\tExpected 1 items but contained 0")
 }
 
 func TestAddBookWithInvalidContentType(t *testing.T) {
@@ -107,6 +86,41 @@ func TestAddBookWithInvalidContentType(t *testing.T) {
 	t.Log("The response should be 415 Unsupported Media Type")
 	if rsp.StatusCode != 415 {
 		t.Errorf("\tExpected 415 but was %v", rsp.StatusCode)
+	}
+
+	t.Log("The response body should be empty")
+	if body != "" {
+		t.Errorf("\tExpected \"\" but was %v", body)
+	}
+
+	t.Log("The comic should not be persisted")
+	actualEvents := store.GetAllEvents()
+	if len(actualEvents) != 0 {
+		t.Errorf("\tExpected 0 items but contained %v", actualEvents)
+	}
+}
+
+func TestAddBookWithInvalidJSON(t *testing.T) {
+	t.Log("POST /books")
+
+	store := persistence.NewInMemoryEventStore()
+	exports := lib.Configure(store)
+	server := httptest.NewServer(exports.Handler)
+	defer server.Close()
+
+	reader := strings.NewReader(`complete-and-utter-rubbish`)
+	rsp, err := http.Post(server.URL+"/books", "application/json", reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rsp.Body.Close()
+	bodyBytes, _ := ioutil.ReadAll(rsp.Body)
+	body := string(bodyBytes)
+
+	t.Log("The response should be 400 Bad Request")
+	if rsp.StatusCode != 400 {
+		t.Errorf("\tExpected 400 but was %v", rsp.StatusCode)
 	}
 
 	t.Log("The response body should be empty")
@@ -182,4 +196,27 @@ func TestGet(t *testing.T) {
 	if body != "" {
 		t.Errorf("\tExpected \"\" but was %v", body)
 	}
+}
+
+func writeMetadata(w *multipart.Writer, metadata string) {
+	metadataHeader := make(textproto.MIMEHeader)
+	metadataHeader.Set("Content-Disposition", "form-data; name=\"metadata\"")
+	metadataHeader.Set("Content-Type", "application/json")
+	metadataPart, _ := w.CreatePart(metadataHeader)
+	metadataPart.Write([]byte(metadata))
+}
+
+func writeImageData(t *testing.T, w *multipart.Writer, imagepath string) {
+	pageImageHeader := make(textproto.MIMEHeader)
+	pageImageHeader.Set("Content-Disposition", "form-data; name=\"page\"")
+	pageImageHeader.Set("Content-Type", "image/jpeg")
+
+	abs, _ := filepath.Abs(imagepath)
+	contents, err := ioutil.ReadFile(abs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	imagePart, _ := w.CreatePart(pageImageHeader)
+	imagePart.Write(contents)
 }
