99fd831ca76a6d58d0fdd3916db873ee47a01b5d
Matt Fenelon <matt@fnln.me>
2013-11-06 13:12:48 +0000

    Removed the dependency on InMemoryFileStore from FakeFileStore.
    I'm changing InMemoryFileStore to read the source files and it won't play nice with the unit tests.

--- src/domain_tests/fakes.go
+++ src/domain_tests/fakes.go
@@ -13,11 +13,28 @@ func NewFakeEventStorer() *FakeEventStorer {
 }
 
 type FakeFileStore struct {
-	*persistence.InMemoryFileStore
+	stored map[string][]string
 }
 
 func NewFakeFileStore() *FakeFileStore {
 	return &FakeFileStore{
-		InMemoryFileStore: persistence.NewInMemoryFileStore(),
+		stored: map[string][]string{},
 	}
+}
+
+func (store *FakeFileStore) Store(key string, files map[string]string) error {
+	names := make([]string, 0, len(files))
+	for f, _ := range files {
+		names = append(names, f)
+	}
+	store.stored[key] = names
+	return nil
+}
+
+func (store *FakeFileStore) Get(key string) []string {
+	return store.stored[key]
+}
+
+func (store *FakeFileStore) GetAll() map[string][]string {
+	return store.stored
 }
