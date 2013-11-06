de81fee6e333eb3b086fe6d20638887369851a2b
Matt Fenelon <matt@fnln.me>
2013-11-06 18:44:16 +0000

    Now using a real FileStore in the HTTP API.

--- src/Gotham/main.go
+++ src/Gotham/main.go
@@ -4,11 +4,16 @@ import (
 	"gotham/lib"
 	"log"
 	"net/http"
+	"persistence/filestore"
 	"persistence/riak"
 )
 
 func main() {
-	exports := lib.Configure(riak.NewRiakEventStore([]string{"127.0.0.1:8087"}, "httpapi"))
+	log.Println("Gotham is starting...")
+
+	eventstore := riak.NewRiakEventStore([]string{"127.0.0.1:8087"}, "httpapi")
+	filestore := filestore.NewLocalFileStore("c:\\gothamfs")
+	exports := lib.Configure(eventstore, filestore)
 
 	err := http.ListenAndServe(":7001", exports.Handler)
 	if err != nil {
