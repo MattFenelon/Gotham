0eb95f584672d5076b07dad1907dd202d470b83e
Matt Fenelon <matt@fnln.me>
2013-11-25 11:43:43 +0000

    Moved the code for creating/updating the FrontPageView into the frontPageViewStore.
    Less for the addComicView method to know and do.

--- src/domain/addcomic.go
+++ src/domain/addcomic.go
@@ -7,7 +7,7 @@ import (
 	"log"
 )
 
-func addComic(newId uuid.UUID, seriesTitle, bookTitle string, pages []string, pageSources []string, eventstorer EventStorer, filestorer FileStorer, fpVs frontPageViewStore, comicVs *comicViewStore) error {
+func addComic(newId uuid.UUID, seriesTitle, bookTitle string, pages []string, pageSources []string, eventstorer EventStorer, filestorer FileStorer, fpVs *frontPageViewStore, comicVs *comicViewStore) error {
 	series, err := model.NewSeriesTitle(seriesTitle)
 	if err != nil {
 		return err
@@ -33,29 +33,7 @@ func addComic(newId uuid.UUID, seriesTitle, bookTitle string, pages []string, pa
 	eventstorer.AddEvent(event)                             // TODO: Deal with errors from the eventstorer
 	filestorer.Store(event.Id.String(), pages, pageSources) // TODO: Deal with errors from the filestorer
 	comicVs.Store(event)
-	saveFrontPage(fpVs, event)
+	fpVs.Store(event)
 
 	return nil
-}
-
-func saveFrontPage(vs frontPageViewStore, event *model.ComicAdded) {
-	frontPage := vs.Get()
-
-	for i, s := range frontPage.Series {
-		if s.Title == event.SeriesTitle.String() {
-			frontPage.Series[i].ImageKey = event.Id.String() + "/" + event.Pages[0]
-			vs.Store(&frontPage)
-			return
-		}
-	}
-
-	newseries := []FrontPageViewSeries{
-		FrontPageViewSeries{
-			Title:    event.SeriesTitle.String(),
-			ImageKey: event.Id.String() + "/" + event.Pages[0],
-		},
-	}
-
-	frontPage.Series = append(newseries, frontPage.Series...)
-	vs.Store(&frontPage)
 }
--- src/domain/frontpageviewstore.go
+++ src/domain/frontpageviewstore.go
@@ -1,19 +1,45 @@
 package domain
 
+import (
+	"domain/model"
+)
+
 type frontPageViewStore struct {
 	vs ViewGetStorer
 }
 
-func newFrontPageViewStore(vs ViewGetStorer) frontPageViewStore {
-	return frontPageViewStore{vs}
+func newFrontPageViewStore(vs ViewGetStorer) *frontPageViewStore {
+	return &frontPageViewStore{vs}
 }
 
-func (vs frontPageViewStore) Store(v *FrontPageView) {
-	vs.vs.Store("frontpage", v) // TODO: Check for errors
+func (viewStore *frontPageViewStore) Store(event *model.ComicAdded) {
+	fp := viewStore.Get()
+
+	for i, s := range fp.Series {
+		if s.Title == event.SeriesTitle.String() {
+			fp.Series[i].ImageKey = event.Id.String() + "/" + event.Pages[0]
+			viewStore.doStore(&fp)
+			return
+		}
+	}
+
+	newseries := []FrontPageViewSeries{
+		FrontPageViewSeries{
+			Title:    event.SeriesTitle.String(),
+			ImageKey: event.Id.String() + "/" + event.Pages[0],
+		},
+	}
+
+	fp.Series = append(newseries, fp.Series...)
+	viewStore.doStore(&fp)
 }
 
-func (vs frontPageViewStore) Get() FrontPageView {
+func (s *frontPageViewStore) doStore(fp *FrontPageView) {
+	s.vs.Store("frontpage", fp) // TODO: Check for errors
+}
+
+func (s *frontPageViewStore) Get() FrontPageView {
 	fp := FrontPageView{Series: []FrontPageViewSeries{}}
-	vs.vs.Get("frontpage", &fp) // TODO: Check for errors
+	s.vs.Get("frontpage", &fp) // TODO: Check for errors
 	return fp
 }
