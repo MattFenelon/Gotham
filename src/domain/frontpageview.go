168247dd1b3dca318fd2d43787dd15c0ae9d6c5f
Matt Fenelon <matt@fnln.me>
2013-12-09 10:58:53 +0000

    Moved the FrontPageViewStore into the frontpageview.go file.
    This is how the other viewstores are organised.

--- src/domain/frontpageview.go
+++ src/domain/frontpageview.go
@@ -1,5 +1,9 @@
 package domain
 
+import (
+	"domain/model"
+)
+
 type FrontPageView struct {
 	Series []FrontPageViewSeries
 }
@@ -8,4 +12,46 @@ type FrontPageViewSeries struct {
 	Title          string
 	ImageKey       string
 	PromotedBookId string
+}
+
+type frontPageViewStore struct {
+	vs ViewGetStorer
+}
+
+func newFrontPageViewStore(vs ViewGetStorer) *frontPageViewStore {
+	return &frontPageViewStore{vs}
+}
+
+func (viewStore *frontPageViewStore) Store(event *model.ComicAdded) {
+	fp := viewStore.Get()
+
+	for i, s := range fp.Series {
+		if s.Title == event.SeriesTitle.String() {
+			fp.Series[i].ImageKey = event.Id.String() + "/" + event.Pages[0]
+			fp.Series[i].PromotedBookId = event.Id.String()
+			viewStore.doStore(&fp)
+			return
+		}
+	}
+
+	newseries := []FrontPageViewSeries{
+		FrontPageViewSeries{
+			Title:          event.SeriesTitle.String(),
+			ImageKey:       event.Id.String() + "/" + event.Pages[0],
+			PromotedBookId: event.Id.String(),
+		},
+	}
+
+	fp.Series = append(newseries, fp.Series...)
+	viewStore.doStore(&fp)
+}
+
+func (s *frontPageViewStore) doStore(fp *FrontPageView) {
+	s.vs.Store("frontpage", fp) // TODO: Check for errors
+}
+
+func (s *frontPageViewStore) Get() FrontPageView {
+	fp := FrontPageView{Series: []FrontPageViewSeries{}}
+	s.vs.Get("frontpage", &fp) // TODO: Check for errors
+	return fp
 }
