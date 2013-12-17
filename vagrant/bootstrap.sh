79a8dad15339ff3a72ac3c27267712ecfde02342
Matt Fenelon <matt@fnln.me>
2013-12-17 16:43:27 +0000

    Update the machine dependencies when it starts.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
+
 wget --no-verbose http://s3.amazonaws.com/downloads.basho.com/riak/1.4/1.4.2/ubuntu/precise/riak_1.4.2-1_amd64.deb
 sudo dpkg -i riak_1.4.2-1_amd64.deb
 
