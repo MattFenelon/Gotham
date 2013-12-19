fb407b7f1acf7be6d72ca046462e1935b37b7dd5
Matt Fenelon <matt@fnln.me>
2013-12-19 11:00:01 +0000

    Gotham API now runs as an Upstart service.

--- scripts/gotham_deploy.sh
+++ scripts/gotham_deploy.sh
@@ -6,5 +6,5 @@ pushd /vagrant/src/gotham
 go install
 popd
 
-scp -C -i /ssh_keys/digital_ocean /vagrant/scripts/install.sh /vagrant/bin/gotham root@162.243.69.92:~/
-ssh -i /ssh_keys/digital_ocean root@162.243.69.92 '~/install.sh'
+scp -Cri /ssh_keys/digital_ocean /vagrant/scripts/gotham_config /vagrant/scripts/gotham_install.sh /vagrant/bin/gotham root@162.243.69.92:~/
+ssh -i /ssh_keys/digital_ocean root@162.243.69.92 '~/gotham_install.sh'
--- scripts/gotham_install.sh
+++ scripts/gotham_install.sh
@@ -4,8 +4,10 @@
 # It is expected that the compiled API is contained in the same
 # location as this script.
 
-pkill gotham
+stop gotham -q
 rm -Rf /usr/local/gotham
 
 mkdir /usr/local/gotham
 cp gotham /usr/local/gotham
+
+rsync -rv gotham_config/ /
